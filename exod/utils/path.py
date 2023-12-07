7073850db927191c0e9ae008681419d3baf24dc9
Norman Khan <nkhan@irll2126h3.irap.omp.eu>
2023-12-07 10:08:03 +0100

    updated paths to use env variable

--- exod/utils/path.py
+++ exod/utils/path.py
@@ -1,40 +1,16 @@
-"""
-EXOD2/ 
-│
-├── data/
-│   ├── raw/
-│   │   ├── downloaded_data/
-│   │   │   ├── observation_id_1/
-│   │   │   ├── observation_id_2/
-│   ├── processed/
-│   │       ├── observation_id_1/
-│   │       ├── observation_id_2/
-│   └── results/
-│       ├── observation_id_1/
-│       │   ├── result_file_1.csv
-│       │   ├── result_file_2.fits
-│       ├── observation_id_2/
-│       │   ├── result_file_1.txt
-│       │   ├── result_file_2.txt
-├── logs/
-│   ├── download_logs/
-│   ├── process_logs/
-│   └── results_logs/
-"""
+import os
 from pathlib import Path
 from exod.utils.logger import logger
 
-module         = Path(__file__).parent.parent            # contains all .py files : /EXOD2/exod
-base           = module.parent             # Top level directory    : /EXOD2/
-data           = base / 'data'      # Data Path              : /EXOD2/data
-data_raw       = data / 'raw'              # This should point to folder with observations eg raw/0001730201
-data_processed = data / 'processed'        # This will contain processed files from the raw observation processed/0001730201
-data_results   = data / 'results'          # This file will contain the results for each obsid results/
-data_combined  = data / 'results_combined' # Contains Combined results from /results/
-logs           = base / 'logs'             # Contains log files.
+base           = Path(os.environ['EXOD'])
+data           = base / 'data'
+data_raw       = data / 'raw'
+data_processed = data / 'processed'
+data_results   = data / 'results'
+data_combined  = data / 'results_combined'
+logs           = base / 'logs'
 
 all_paths = {
-    'module': module,
     'base': base,
     'data': data,
     'data_raw': data_raw,
