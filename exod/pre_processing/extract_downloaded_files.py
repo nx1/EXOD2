b7037df2d9d5d08db530fc651e1a586bf3992418
Norman Khan <nkhan@irll2126h3.irap.omp.eu>
2023-12-08 15:23:52 +0100

    updated download, extract and setup

---
+++ exod/pre_processing/extract_downloaded_files.py
@@ -0,0 +1,29 @@
+from glob import glob
+import tarfile
+from exod.utils import path
+from exod.utils.logger import logger
+
+logger.info('Extracting Downloaded files')
+dl_path = path.data_downloaded
+extract_path = path.data_raw
+logger.info(f'Looking in Path: {dl_path}')
+logger.info(f'Extraction_path: {extract_path}')
+
+
+wildcard = f'{dl_path}/*.tar'
+
+tar_gz_files = glob(wildcard)
+
+N_files = len(tar_gz_files)
+if N_files == 0:
+    logger.info('No files found to extract')
+    exit()
+
+logger.info(f'.tar.gz files to process: {N_files} press any key to start Ctrl+C to exit')
+
+for f in tar_gz_files:
+    logger.info(f'extracting file: {f} --> {extract_path}')
+    with tarfile.open(f, 'r') as tar:
+        tar.extractall(extract_path)
+
+
--- exod/utils/path.py
+++ exod/utils/path.py
@@ -2,17 +2,19 @@ import os
 from pathlib import Path
 from exod.utils.logger import logger
 
-base           = Path(os.environ['EXOD'])
-data           = base / 'data'
-data_raw       = data / 'raw'
-data_processed = data / 'processed'
-data_results   = data / 'results'
-data_combined  = data / 'results_combined'
-logs           = base / 'logs'
+base            = Path(os.environ['EXOD'])
+data            = base / 'data'
+data_downloaded = data / 'data_downloaded'
+data_raw        = data / 'raw'
+data_processed  = data / 'processed'
+data_results    = data / 'results'
+data_combined   = data / 'results_combined'
+logs            = base / 'logs'
 
 all_paths = {
     'base': base,
     'data': data,
+    'data_downloaded' : data_downloaded,
     'data_raw': data_raw,
     'data_processed': data_processed,
     'data_results': data_results,
@@ -20,7 +22,13 @@ all_paths = {
     'logs': logs
 }
 
+def create_all_paths():
+    for name, path in all_paths.items():
+        logger.info(f'Creating Path: {path}')
+        os.makedirs(path, exist_ok=True)
+
 if __name__ == "__main__":
+    create_all_paths()
     for name, path in all_paths.items():
         exists = "exists" if path.exists() else "does not exist"
         logger.info(f"{name:<15} : {path} : {exists}")
