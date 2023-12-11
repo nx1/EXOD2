aae215cd38ea3a0ffa2a36a7f807e1d6851d4285
ErwanQuintin <erwan.quintin@hotmail.fr>
2023-12-11 16:24:47 +0100

    Added a match with GLADE+ and Gaia

--- exod/post_processing/match_transient_candidates.py
+++ exod/post_processing/match_transient_candidates.py
@@ -1,8 +1,10 @@
-#TODO: Match the candidate transients with Simbad and archival X-ray catalogs (from STONKS?)
-from astropy.coordinates import SkyCoord
+#TODO: Match the candidate transients with archival X-ray catalogs (from STONKS?)
+from astropy.coordinates import SkyCoord, Angle, angular_separation
 import astropy.units as u
 from astroquery.simbad import Simbad
 Simbad.add_votable_fields("otype")
+from astroquery.vizier import Vizier
+
 from exod.utils.classes_Simbad import simbad_classifier
 
 
@@ -10,10 +12,8 @@ def simbad_lookup(tab_ra, tab_dec):
     tab_names=[]
     tab_types=[]
     for (ra, dec) in zip(tab_ra, tab_dec):
-        result_table = Simbad.query_region(SkyCoord(ra, dec,
-                                                    unit=(u.deg, u.deg), frame='icrs'),
+        result_table = Simbad.query_region(SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'),
                                            radius=10 * u.arcsec)
-        print(result_table)
         if result_table is not None:
             result = result_table[0]
             tab_names.append(result["MAIN_ID"])
@@ -23,6 +23,46 @@ def simbad_lookup(tab_ra, tab_dec):
             tab_types.append("")
     return tab_names, tab_types
 
+def GLADE_lookup(tab_ra, tab_dec):
+    tab_GLADE_ID=[]
+    tab_dist=[]
+    tab_mass=[]
+    for (ra, dec) in zip(tab_ra, tab_dec):
+        result_table = Vizier.query_region(SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs'),
+                                           radius=10 * u.arcsec, catalog='GLADE+')
+        if len(result_table)>0:
+            result = result_table[0]
+            tab_GLADE_ID.append(result["GLADE_"][0])
+            tab_dist.append(result["dL"][0])
+            tab_mass.append(result["M_"][0])
+        else:
+            tab_GLADE_ID.append("")
+            tab_dist.append("")
+            tab_mass.append("")
+    return tab_GLADE_ID, tab_dist, tab_mass
+
+def Gaia_lookup(tab_ra, tab_dec):
+    tab_Gaia_name=[]
+    tab_dist=[]
+    tab_sep=[]
+    for (ra, dec) in zip(tab_ra, tab_dec):
+        result_table = Vizier.query_region(SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs'),
+                                           radius=10 * u.arcsec, catalog='I/355/gaiadr3')
+        if len(result_table)>0:
+            result = result_table[0]
+            tab_Gaia_name.append(list(result["Source"]))
+            tab_dist.append(list(result["Dist"].value))
+            tab_sep.append(SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg), frame='icrs').separation(
+                SkyCoord(ra=result["RA_ICRS"], dec=result["DE_ICRS"], unit=(u.deg, u.deg), frame='icrs')
+                                              ).to(u.arcsec).value)
+        else:
+            tab_Gaia_name.append("")
+            tab_dist.append("")
+            tab_sep.append("")
+    return tab_Gaia_name, tab_dist,tab_sep
+
 if __name__=='__main__':
-    tab_ra, tab_dec = [19.786252, 15], [-34.1923,0]
+    tab_ra, tab_dec = [19.786252, 15, 283.3269], [-34.1923,0,+33.04672]
     print(simbad_lookup(tab_ra, tab_dec))
+    print(GLADE_lookup(tab_ra, tab_dec))
+    print(Gaia_lookup(tab_ra, tab_dec))
