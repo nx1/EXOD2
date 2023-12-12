8551321cd48b54aef0f59fbe4a6a65829032bd24
Norman Khan <nkhan@irll2126h3.irap.omp.eu>
2023-12-12 16:20:25 +0100

    updated download_observation.py

--- exod/pre_processing/download_observations.py
+++ exod/pre_processing/download_observations.py
@@ -12,6 +12,30 @@ def read_observation_ids(file_path):
         obs_ids = [line.strip() for line in file.readlines()]
     return obs_ids
 
+def download_observation_pps(observation_id, save_dir, clobber=False):
+    """
+    Download the post-processed XMM .tar observation files.
+    """
+    download_url = f'https://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno={obs}&level=PPS'
+    file_path = save_dir / f'{obs}.tar'
+
+    if file_path.is_file() and not clobber:
+        logger.info(f'Skipping {file_path}. File already exists.')
+    else:
+        # Use requests to download the file
+        response = requests.get(download_url)
+        logger.info(response)
+        if response.status_code == 200:
+            logger.info('Response 200, downloading to {file_path}')
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+            logger.info(f'Downloaded: {file_path}')
+        else:
+            logger.warning(f'Failed to download: {file_path}')
+
+
+
+clobber=False
 
 obs_list_path = path.data / 'observations.txt'
 output_sh_path = path.data / 'download_obs.sh'
@@ -26,17 +50,5 @@ observation_ids = read_observation_ids(obs_list_path)
 logger.info(f'Found {len(observation_ids)} observations ids')
 
 for obs in observation_ids:
-    download_url = f'https://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno={obs}&level=PPS'
-    file_path = save_dir / f'{obs}.tar'
-
-    # Use requests to download the file
-    response = requests.get(download_url)
-    logger.info(response)
-    if response.status_code == 200:
-        logger.info('Response 200, downloading to {file_path}')
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
-        logger.info(f'Downloaded: {file_path}')
-    else:
-        logger.warning(f'Failed to download: {file_path}')
+    download_observation_pps(observation_id=obs, save_dir=save_dir, clobber=clobber)
 
