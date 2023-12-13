8f625ed05565dff606562d27038e2bd8fd63f0bd
Norman Khan <nkhan@irll2126h3.irap.omp.eu>
2023-12-13 10:30:56 +0100

    name main dl

--- exod/pre_processing/download_observations.py
+++ exod/pre_processing/download_observations.py
@@ -34,21 +34,21 @@ def download_observation_pps(observation_id, save_dir, clobber=False):
             logger.warning(f'Failed to download: {file_path}')
 
 
-
-clobber=False
-
-obs_list_path = path.data / 'observations.txt'
-output_sh_path = path.data / 'download_obs.sh'
-save_dir = path.data_downloaded
-
-logger.info(f'Observations List Path: {obs_list_path}')
-logger.info(f'Download Script Path: {output_sh_path}')
-logger.info(f'Save Directory: {save_dir}')
-
-logger.info(f'Reading observations from {obs_list_path}')
-observation_ids = read_observation_ids(obs_list_path)
-logger.info(f'Found {len(observation_ids)} observations ids')
-
-for obs in observation_ids:
-    download_observation_pps(observation_id=obs, save_dir=save_dir, clobber=clobber)
+if __name__ == "__main__":
+    clobber=False
+    
+    obs_list_path = path.data / 'observations.txt'
+    output_sh_path = path.data / 'download_obs.sh'
+    save_dir = path.data_downloaded
+    
+    logger.info(f'Observations List Path: {obs_list_path}')
+    logger.info(f'Download Script Path: {output_sh_path}')
+    logger.info(f'Save Directory: {save_dir}')
+    
+    logger.info(f'Reading observations from {obs_list_path}')
+    observation_ids = read_observation_ids(obs_list_path)
+    logger.info(f'Found {len(observation_ids)} observations ids')
+    
+    for obs in observation_ids:
+        download_observation_pps(observation_id=obs, save_dir=save_dir, clobber=clobber)
 
