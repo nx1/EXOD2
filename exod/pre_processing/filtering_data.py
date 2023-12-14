e8734753c668f6084e0801388219cdf717c43ae5
Norman Khan <norman.khan@irap.omp.eu>
2023-12-14 10:45:11 +0100

    mades changes to filtering_data

--- exod/pre_processing/filtering_data.py
+++ exod/pre_processing/filtering_data.py
@@ -4,122 +4,121 @@ Requires having pre-set 'setsas' and 'export CCFPATH=' in the terminal.
 import os
 import shlex
 import subprocess
+
 from exod.utils.path import data_raw, data_processed
+from exod.utils.logger import logger
 
-def filter_events_file(obs, min_energy=0.2, max_energy=12.):
-    """Filtering scripts for all EPIC data.
-    Takes the energy range in keV and observation name as arguments"""
+def run_cmd(cmd):
+    logger.info('Running Command with os.system:')
+    logger.info(cmd)
+    ret = os.system(cmd)
+    logger.info(f'Return Code: {ret}')
+    if ret != 0:
+        raise OSError(f'Failed to run command!: {cmd} \n Return code: {ret}')
+    else:
+        return ret
 
-    print(f'Filtering observation {obs}...')
-    os.makedirs(os.path.join(data_processed, obs), exist_ok=True)
+def get_raw_and_processed_obs_path(obs):
+    path_raw_obs       = data_raw / f'{obs}'
+    path_processed_obs = data_processed / f'{obs}'
+    os.makedirs(path_processed_obs, exist_ok=True)
+    return path_raw_obs, path_processed_obs
 
-    min_PI = int(min_energy*1000)
-    max_PI = int(max_energy*1000)
+def filter_PN_file(obs, min_energy=0.2, max_energy=12.):
+    path_raw_obs, path_processed_obs = get_raw_and_processed_obs_path(obs)
+    raw_PN_file        = list(path_raw_obs.glob('*PIEVLI*'))[0]
+    clean_PN_file      = path_processed_obs / f'PN_pattern_clean.fits'
+    imagePN            = path_processed_obs / f'PN_image.fits'
 
-    raw_PN_file = os.path.join(data_raw,obs,f'P{obs}PNS001PIEVLI.FTZ')
-    clean_PN_file = os.path.join(data_processed, obs, f'PN_pattern_clean.fits')
-    imagePN = os.path.join(data_processed, obs, f'PN_image.fits')
+    min_PI, max_PI = int(min_energy*1000), int(max_energy*1000)
+
     cmd=(f'evselect table={raw_PN_file} withfilteredset=Y filteredset={clean_PN_file} destruct=Y keepfilteroutput=T '
          f'expression="#XMMEA_EP && (PATTERN<=4) && (PI in [{min_PI}:{max_PI}])" -V 0')
-    os.system(cmd)
+    run_cmd(cmd)
+
     cmd=(f'evselect table={clean_PN_file} imagebinning=binSize imageset={imagePN} withimageset=yes xcolumn=X ycolumn=Y'
          f' ximagebinsize=80 yimagebinsize=80 -V 0')
-    os.system(cmd)
+    run_cmd(cmd)
 
-    raw_M1_file = os.path.join(data_raw,obs,f'P{obs}M1S002MIEVLI.FTZ')
-    clean_M1_file = os.path.join(data_processed, obs, f'M1_pattern_clean.fits')
-    imageM1 = os.path.join(data_processed, obs, f'M1_image.fits')
+def filter_M1_file(obs, min_energy=0.2, max_energy=12.):
+    path_raw_obs, path_processed_obs = get_raw_and_processed_obs_path(obs)
+    raw_M1_file        = list(path_raw_obs.glob('*M1*EVLI*'))[0]
+    clean_M1_file      = path_processed_obs / f'M1_pattern_clean.fits'
+    imageM1            = path_processed_obs / f'M1_image.fits'
+
+    min_PI, max_PI = int(min_energy*1000), int(max_energy*1000)
+
     cmd=(f'evselect table={raw_M1_file} withfilteredset=Y filteredset={clean_M1_file} destruct=Y keepfilteroutput=T '
          f'expression="#XMMEA_EM && (PATTERN<=12) && (PI in [{min_PI}:{max_PI}])" -V 0')
-    os.system(cmd)
+    run_cmd(cmd)
+
     cmd=(f'evselect table={clean_M1_file} imagebinning=binSize imageset={imageM1} withimageset=yes xcolumn=X ycolumn=Y'
          f' ximagebinsize=80 yimagebinsize=80 -V 0')
-    os.system(cmd)
+    run_cmd(cmd)
 
 
-    raw_M2_file = os.path.join(data_raw,obs,f'P{obs}M2S003MIEVLI.FTZ')
-    clean_M2_file = os.path.join(data_processed, obs, f'M2_pattern_clean.fits')
-    imageM2 = os.path.join(data_processed, obs, f'M2_image.fits')
+def filter_M2_file(obs, min_energy=0.2, max_energy=12.):
+    path_raw_obs, path_processed_obs = get_raw_and_processed_obs_path(obs)
+    raw_M2_file        = list(path_raw_obs.glob('*M1*EVLI*'))[0]
+    clean_M2_file      = path_processed_obs / f'M2_pattern_clean.fits'
+    imageM2            = path_processed_obs / f'M2_image.fits'
+
+    min_PI, max_PI = int(min_energy*1000), int(max_energy*1000)
+
     cmd=(f'evselect table={raw_M2_file} withfilteredset=Y filteredset={clean_M2_file} destruct=Y keepfilteroutput=T '
          f'expression="#XMMEA_EM && (PATTERN<=12) && (PI in [{min_PI}:{max_PI}])" -V 0')
-    os.system(cmd)
+    run_cmd(cmd)
+
     cmd=(f'evselect table={clean_M2_file} imagebinning=binSize imageset={imageM2} withimageset=yes xcolumn=X ycolumn=Y'
          f' ximagebinsize=80 yimagebinsize=80 -V 0')
-    os.system(cmd)
+    run_cmd(cmd)
 
 
-def filter_events_file_gti_only(obs, min_energy=0.2, max_energy=10.):
-    """Filtering scripts for all EPIC data, keeping only GTIs.
-     Requires having pre-set 'setsas' and 'export CCFPATH=' in the terminal.
-    Takes the energy range in keV and observation name as arguments"""
 
-    print(f'Filtering observation {obs}...')
-    os.makedirs(os.path.join(data_processed, obs), exist_ok=True)
+def filter_observation_events(obs, min_energy=0.2, max_energy=12.):
+    """
+    Filter observation event lists for PN, M1 and M2.
 
-    min_PI = int(min_energy*1000)
-    max_PI = int(max_energy*1000)
+    Parameters:
+    - obs (str): Observation identifier.
+    - min_energy (float): Minimum energy threshold.
+    - max_energy (float): Maximum energy threshold.
 
-    raw_PN_file = os.path.join(data_raw,obs,f'P{obs}PNS001PIEVLI.FTZ')
-    PN_highE_rates = os.path.join(data_processed,obs,f'PN_highE_rates.fits')
-    PN_gti_file = os.path.join(data_processed,obs,f'PN_gti.fits')
-    clean_PN_file = os.path.join(data_processed, obs, f'PN_clean.fits')
-    imagePN = os.path.join(data_processed, obs, f'PN_image.fits')
-    #Extracting high energy rates
-    cmd=(f'evselect table={raw_PN_file} withrateset=Y rateset={PN_highE_rates} maketimecolumn=Y timebinsize=100 '
-         f'makeratecolumn=Y expression="#XMMEA_EP && (PI in [10000:12000]) && (PATTERN==0)" -V 0')
-    os.system(cmd)
-    #Computing GTIs
-    cmd=(f'tabgtigen table={PN_highE_rates} expression="RATE<=0.5" gtiset={PN_gti_file} -V 0')
-    os.system(cmd)
-    #Cleaning events list
-    cmd=(f'evselect table={raw_PN_file} withfilteredset=Y filteredset={clean_PN_file} destruct=Y keepfilteroutput=T '
-         f'expression="#XMMEA_EP && gti({PN_gti_file},TIME) && (PATTERN<=4) && (PI in [{min_PI}:{max_PI}])" -V 0')
-    os.system(cmd)
-    cmd=(f'evselect table={clean_PN_file} imagebinning=binSize imageset={imagePN} withimageset=yes xcolumn=X ycolumn=Y'
-         f' ximagebinsize=80 yimagebinsize=80 -V 0')
-    os.system(cmd)
+    Returns:
+    - dict: Dictionary indicating the status of each filter.
+    """
 
-    raw_M1_file = os.path.join(data_raw,obs,f'P{obs}M1S002MIEVLI.FTZ')
-    M1_highE_rates = os.path.join(data_processed,obs,f'M1_highE_rates.fits')
-    M1_gti_file = os.path.join(data_processed,obs,f'M1_gti.fits')
-    clean_M1_file = os.path.join(data_processed, obs, f'M1_clean.fits')
-    imageM1 = os.path.join(data_processed, obs, f'M1_image.fits')
-    #Extracting high energy rates
-    cmd=(f'evselect table={raw_M1_file} withrateset=Y rateset={M1_highE_rates} maketimecolumn=Y timebinsize=100 '
-         f'makeratecolumn=Y expression="#XMMEA_EM && (PI>10000) && (PATTERN==0)" -V 0')
-    os.system(cmd)
-    # Computing GTIs
-    cmd=(f'tabgtigen table={M1_highE_rates} expression="RATE<=0.4" gtiset={M1_gti_file} -V 0')
-    os.system(cmd)
-    # Cleaning events list
-    cmd=(f'evselect table={raw_M1_file} withfilteredset=Y filteredset={clean_M1_file} destruct=Y keepfilteroutput=T '
-         f'expression="#XMMEA_EM && (PATTERN<=12) && (PI in [{min_PI}:{max_PI}])" -V 0')
-    os.system(cmd)
-    cmd=(f'evselect table={clean_M1_file} imagebinning=binSize imageset={imageM1} withimageset=yes xcolumn=X ycolumn=Y'
-         f' ximagebinsize=80 yimagebinsize=80 -V 0')
-    os.system(cmd)
+    filter_result = {'obsid':obs,'PN': 'Not Run', 'M1': 'Not Run', 'M2': 'Not Run'}
 
+    filters = [
+        {'name': 'PN', 'function': filter_PN_file},
+        {'name': 'M1', 'function': filter_M1_file},
+        {'name': 'M2', 'function': filter_M2_file}
+    ]
 
-    raw_M2_file = os.path.join(data_raw,obs,f'P{obs}M2S003MIEVLI.FTZ')
-    M2_highE_rates = os.path.join(data_processed,obs,f'M2_highE_rates.fits')
-    M2_gti_file = os.path.join(data_processed,obs,f'M2_gti.fits')
-    clean_M2_file = os.path.join(data_processed, obs, f'M2_clean.fits')
-    imageM2 = os.path.join(data_processed, obs, f'M2_image.fits')
-    #Extracting high energy rates
-    cmd=(f'evselect table={raw_M2_file} withrateset=Y rateset={M2_highE_rates} maketimecolumn=Y timebinsize=100 '
-         f'makeratecolumn=Y expression="#XMMEA_EM && (PI>10000) && (PATTERN==0)" -V 0')
-    os.system(cmd)
-    # Computing GTIs
-    cmd=(f'tabgtigen table={M2_highE_rates} expression="RATE<=0.4" gtiset={M2_gti_file} -V 0')
-    os.system(cmd)
-    # Cleaning events list
-    cmd=(f'evselect table={raw_M2_file} withfilteredset=Y filteredset={clean_M2_file} destruct=Y keepfilteroutput=T '
-         f'expression="#XMMEA_EM && (PATTERN<=12) && (PI in [{min_PI}:{max_PI}])" -V 0')
-    os.system(cmd)
-    cmd=(f'evselect table={clean_M2_file} imagebinning=binSize imageset={imageM2} withimageset=yes xcolumn=X ycolumn=Y'
-         f' ximagebinsize=80 yimagebinsize=80 -V 0')
-    os.system(cmd)
+    for f in filters:
+        try:
+            f['function'](obs, min_energy=min_energy, max_energy=max_energy)
+            filter_result[f['name']] = 'Success'
+        except IndexError as e:
+            logger.info(f'Could not process {f["name"]} event file for obs={obs} {e}')
+            filter_result[f['name']] = 'IndexError'
+
+    return filter_result
+
 
 if __name__ == "__main__":
-    #filter_events_file('0831790701', min_energy=0.2, max_energy=12)
-    filter_events_file_gti_only('0831790701', min_energy=0.2, max_energy=2)
+    from exod.pre_processing.download_observations import read_observation_ids
+    from exod.utils.path import data
+    import pandas as pd
+
+    obsids = read_observation_ids(data / 'observations.txt')
+    all_res = []
+    for obs in obsids:
+        res = filter_observation_events(obs, min_energy=0.2, max_energy=12.)
+        all_res.append(res)
+
+    df = pd.DataFrame(all_res)
+    print(df)
+
+  
