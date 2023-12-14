7028fb7406bc5ee514c5735a609a9bc0c72593cb
Norman Khan <norman.khan@irap.omp.eu>
2023-12-14 10:15:20 +0100

    added a second logger format

--- exod/utils/logger.py
+++ exod/utils/logger.py
@@ -1,7 +1,9 @@
 import logging
 
 # Configure the logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - %(message)s')
+fmt1 = '%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - %(message)s'
+fmt2 = '%(levelname)s - %(asctime)s - %(message)s' 
+logging.basicConfig(level=logging.INFO, format=fmt2)
 logging.getLogger('matplotlib.font_manager').disabled = True
 
 # Create a logger instance
