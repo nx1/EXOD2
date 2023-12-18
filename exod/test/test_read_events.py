ab1950bfb85a0b33f20818dbe2d32c04ac80cc14
Norman Khan <norman.khan@irap.omp.eu>
2023-12-18 18:21:30 +0100

    added test folder

---
+++ exod/test/test_read_events.py
@@ -0,0 +1,20 @@
+# Supported SUBMODES
+PN_SUBMODES = {'PrimeFullWindow'         : True,
+               'PrimeFullWindowExtended' : True,
+               'PrimeLargeWindow'        : True,
+               'PrimeSmallWindow'        : True,
+               'FastTiming'              : False,
+               'FastBurst'               : False,
+               'ModifiedTiming'          : False,
+               'Large Offset'            : False}
+
+MOS_SUBMODES = {'PrimeFullWindow'  : True,
+                'PrimePartialRFS'  : True,
+                'PrimePartialW2'   : True,
+                'PrimePartialW3'   : True,
+                'PrimePartialW4'   : True,
+                'PrimePartialW5'   : True,
+                'PrimePartialW6'   : True,
+                'FastUncompressed' : True,
+                'FastCompressed'   : True}
+
