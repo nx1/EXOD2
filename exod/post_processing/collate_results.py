d330afbed6430bdb4af8dbc9f9bd8489bd24b3e7
Norman Khan <norman.khan@irap.omp.eu>
2023-12-22 16:18:14 +0100

    added colate_results

---
+++ exod/post_processing/collate_results.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astroquery.simbad import Simbad
+
+from astroquery.exceptions import BlankResponseWarning
+import warnings
+warnings.simplefilter(action='ignore', category=BlankResponseWarning) # Ignore Non-results.
+from exod.utils.path import data_results
+from exod.utils.logger import logger
+from exod.utils.simbad_classes import simbad_classifier
+
+
+def combine_all_region_files():
+    logger.info('Combining all region files')
+    csv_files = list(data_results.glob('*/*regions.csv'))
+    logger.info(f'Found {len(csv_files)} csv files')
+
+    dfs = []
+    for csv in csv_files:
+        df = pd.read_csv(csv, dtype={'obsid':str})
+        logger.info(f'{csv} rows: {len(df)}')
+        dfs.append(df)
+    df_all_regions = pd.concat(dfs, axis=0).reset_index(drop=True)
+    logger.info(f'df_all_regions:\n{df_all_regions}')
+    return df_all_regions
+
+
+
+def crossmatch_simbad(df_region, radius):
+    logger.info(f'Crossmatching df_region len={len(df_region)} with SIMBAD, radius={radius}')
+
+    logger.info('Creating SkyCoord Object')
+    coords = SkyCoord(ra=df_region['ra'],
+                      dec=df_region['dec'],
+                      unit='deg',
+                      frame='icrs')
+
+    simbad = Simbad()
+    # Additional fields can be checked with simbad.list_votable_fields()
+    simbad.add_votable_fields('otype', 'distance')
+
+
+    logger.info('Querying Region (This can take a while)')
+    tab_res = simbad.query_region(coordinates=coords, radius=radius)
+    tab_res['SCRIPT_NUMBER_ID'] = tab_res['SCRIPT_NUMBER_ID'] - 1  # Use 0 Indexing
+    logger.info(f'Found {len(tab_res)} results')
+
+    logger.info('Appending region coordinates to table...')
+    tab_res['RA_REGION_DEG'] = [coords[i].ra for i in tab_res['SCRIPT_NUMBER_ID']]
+    tab_res['DEC_REGION_DEG'] = [coords[i].dec for i in tab_res['SCRIPT_NUMBER_ID']]
+
+    logger.info('Calculating seperations...')
+    sc1 = SkyCoord(ra=tab_res['RA_REGION_DEG'], dec=tab_res['DEC_REGION_DEG'])
+    sc2 = SkyCoord(ra=tab_res['RA'], dec=tab_res['DEC'], unit=(u.hourangle, u.deg))
+    sep = sc1.separation(sc2)
+    tab_res['SEP'] = sep
+    return tab_res
+
+
+def get_df_regions_no_crossmatch(df_regions, tab_res):
+    l1 = df_all_regions.index
+    l2 = tab_res['SCRIPT_NUMBER_ID']
+    no_crossmatch_indexes = np.setdiff1d(l1, l2)
+    logger.info(f'{len(no_crossmatch_indexes)} / {len(l1)} regions have no counterparts with SIMBAD')
+    df_all_regions_no_crossmatch = df_all_regions.iloc[no_crossmatch_indexes]
+    return df_all_regions_no_crossmatch
+
+
+def classify_simbad_otype(tab_res):
+    classification = [simbad_classifier[t] for t in tab_res['OTYPE']]
+    tab_res['CLASSIFICATION'] = classification
+    return tab_res
+
+
+if __name__ == '__main__':
+    df_all_regions = combine_all_region_files()
+    tab_res = crossmatch_simbad(df_all_regions, radius=1*u.arcmin)
+    tab_res = classify_simbad_otype(tab_res=tab_res)
+    df_all_regions_no_crossmatch = get_df_regions_no_crossmatch(df_regions=df_all_regions, tab_res=tab_res)
